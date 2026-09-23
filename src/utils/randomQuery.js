8bfc39a81b96e9cd1aea71d366b080cbb4758856
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-23 14:32:17 +0530

    dictionary expansion

--- src/utils/randomQuery.js
+++ src/utils/randomQuery.js
@@ -1,9 +1,38 @@
 const queries = [
+  // Common movie words
   'love', 'war', 'dark', 'fire', 'blood',
   'night', 'man', 'king', 'dead', 'lost',
   'rise', 'fall', 'black', 'secret', 'ghost',
   'storm', 'shadow', 'hero', 'last', 'new',
   'city', 'dream', 'red', 'star', 'wild',
+  
+  // Action & Adventure
+  'dragon', 'fight', 'gun', 'mission', 'escape',
+  'hunt', 'revenge', 'attack', 'chase', 'battle',
+  
+  // Drama & Emotion
+  'heart', 'soul', 'life', 'death', 'hope',
+  'fear', 'rage', 'tears', 'forever', 'truth',
+  
+  // Mystery & Thriller
+  'murder', 'killer', 'crime', 'detective', 'spy',
+  'conspiracy', 'danger', 'trap', 'suspect', 'witness',
+  
+  // Fantasy & Sci-Fi
+  'magic', 'alien', 'space', 'future', 'time',
+  'galaxy', 'planet', 'legend', 'curse', 'power',
+  
+  // Horror
+  'evil', 'demon', 'devil', 'haunted', 'nightmare',
+  'zombie', 'monster', 'terror', 'darkness', 'scream',
+  
+  // Romance
+  'kiss', 'wedding', 'bride', 'hearts', 'passion',
+  
+  // Classic movie titles & themes
+  'American', 'beautiful', 'final', 'invisible', 'perfect',
+  'silent', 'golden', 'midnight', 'world', 'girl',
+  'boy', 'mother', 'father', 'brother', 'sister',
 ];
 
 export function getRandomQuery() {
