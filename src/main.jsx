e2d912e0d8dd0396119396feede8a7e681dc3d3f
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-23 13:48:54 +0530

    feat(routing): add ScrollToTop component to reset scroll position on navigation
    - Create ScrollToTop component that listens to route changes via useLocation hook
    - Automatically scroll to top of page (0, 0) whenever pathname changes
    - Wrap App component with ScrollToTop to apply behavior globally
    - Improves user experience by preventing scroll position carryover between pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { WatchlistProvider } from './context/WatchlistContext.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 import './index.css'
 import App from './App.jsx'
@@ -10,7 +11,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <WatchlistProvider>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </WatchlistProvider>
     </BrowserRouter>
   </StrictMode>
